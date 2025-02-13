9fe490c5512143a6bdaea4bbabf3b520d86f53e2
Alessandro Araujo <alessandro.v.b.araujo@gmail.com>
2025-02-13 16:19:09 +0000

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 
 class UserController extends Controller
 {
@@ -41,5 +45,92 @@ class UserController extends Controller
             'status' => true,
             'user' => $user
         ], 200);
+    }
+
+    /**
+     * Vamos retornar um Json
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        try {
+            // Cadastrar o usuário no BD
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+            // Retorna os dados do usuário criado e uma mensagem de sucesso com status 201
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => "Usuário cadastrado com sucesso!",
+            ], 201);
+
+        } catch (QueryException $e) {
+            Log::error('Erro no /auth/create: ' . $e->getMessage());
+            // Retorna uma mensagem de erro com status 400
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não cadastrado!",
+            ], 400);
+        }
+    }
+
+    /**
+     * Atualizar os dados de um usuário existente com base nos dados fornecidos na requisição.
+     *
+     * @param  \App\Http\Requests\UserRequest  $request O objeto de requisição contendo os dados do usuário a ser atualizado.
+     * @param  \App\Models\User  $user O usuário a ser atualizado.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, User $user): JsonResponse
+    {
+        try {
+            // Editar as informações do registro no banco de dados
+            $user->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+            // Retorna os dados do usuário editado e uma mensagem de sucesso com status 200
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => "Usuário editado com sucesso!",
+            ], 200);
+        }catch (Exception $e) {
+            // Retorna uma mensagem de erro com status 400
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não editado!",
+            ], 400);
+        }
+    }
+
+    /**
+     * Vamos retornar um Json
+     * @param \App\Models\User
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(User $user): JsonResponse
+    {
+        try {
+            // Apagar o registro no banco de dados
+            $user->delete();
+            // Retorna os dados do usuário apagado e uma mensagem de sucesso com status 200
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => "Usuário apagado com sucesso!",
+            ], 200);
+        } catch (Exception $e) {
+            // Retorna uma mensagem de erro com status 400
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não apagado!",
+            ], 400);
+        }
     }
 }
